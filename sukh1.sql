374f9e7d4d1abe93e6f3f0df74427343aaf2d50e
sukhdeep <sukhdeep@arintech.in>
2024-03-06 12:01:29 +0000

    lk

--- sukh1.sql
+++ sukh1.sql
@@ -1,6 +1,11 @@
 -- create_table.sql
 USE sukhdeep;
 
+SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = 'sukhdeep'
+AND table_name = 'customers';
+
 CREATE TABLE customers (
     id INT PRIMARY KEY,
     name VARCHAR(255),
