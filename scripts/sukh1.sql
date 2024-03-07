406b9f7dd28f536dc315e193ffa856b30f977959
sukhdeep <sukhdeep@arintech.in>
2024-03-07 11:59:11 +0000

    kj

---
+++ scripts/sukh1.sql
@@ -0,0 +1,16 @@
+-- create_table.sql
+USE sukhdeep;
+
+SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = 'sukhdeep'
+AND table_name = 'customers';
+
+CREATE TABLE IF NOT EXISTS customers (
+CREATE TABLE customers (
+    id INT PRIMARY KEY,
+    name VARCHAR(255),
+    age INT
+    -- Add more columns and constraints as needed
+);
+
